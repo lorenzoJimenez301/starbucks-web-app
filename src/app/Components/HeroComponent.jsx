056cc65e57704e9ebf75af1852873c3d0c3cc371
Lorenzo Jiménez Abarca <ljimenez@TAP.LOCAL>
2025-07-04 17:09:38 -0600

    Preparativos

---
+++ src/app/Components/HeroComponent.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+
+import React from 'react'
+
+const HeroComponent = () => {
+  return (
+    <h1 className='text-black font-bold'>HeroComponent</h1>
+  )
+}
+
+export default HeroComponent
