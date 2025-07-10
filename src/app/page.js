ce035c6d0f5c48c516c980c1c8d725420685f159
Lorenzo Jiménez Abarca <ljimenez@TAP.LOCAL>
2025-07-10 14:31:42 -0600

    Terminada

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,83 @@
-import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
 
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        <Navbar className="bg-transparent!" fluid>
-          <NavbarBrand href="https://flowbite-react.com">
-            <img
-              src="/images/starbucks-icon.svg"
-              className="mr-3 h-14 sm:h-9"
-              alt="Flowbite React Logo"
-            />
-          </NavbarBrand>
-          <div className="flex md:order-2">
-            <NavbarToggle className="cursor-pointer" />
+    <main className="min-h-screen flex flex-col">
+      <section className="container m-auto font-normal">
+        <article className="flex flex-col sm:flex-row w-full ">
+          <div className=" w-2/3">
+            <header>
+              <Navbar className="bg-transparent! sm:p-8 w-max" fluid>
+                <NavbarBrand href="https://flowbite-react.com">
+                  <img
+                    src="/images/starbucks-icon.svg"
+                    className="mr-3 h-14 sm:h-14"
+                    alt="Flowbite React Logo"
+                  />
+                </NavbarBrand>
+                <div className="flex md:order-2">
+                  <NavbarToggle className="cursor-pointer" />
+                </div>
+                <NavbarCollapse className="uppercase items-center sm:ml-14">
+                  <NavbarLink
+                    className="text-green-star! border-none hover:text-white! font-bold!"
+                    href="#"
+                  >
+                    Menu
+                  </NavbarLink>
+                  <NavbarLink
+                    className="text-green-star! border-none hover:text-white! font-bold!"
+                    href="#"
+                  >
+                    Rewards
+                  </NavbarLink>
+                  <NavbarLink
+                    className="text-green-star! border-none hover:text-white! font-bold!"
+                    href="#"
+                  >
+                    Gift Card
+                  </NavbarLink>
+                </NavbarCollapse>
+              </Navbar>
+            </header>
+            <div className="sm:flex sm:flex-col justify-center items-start space-y-4  uppercase font-extrabold h-2/3 ">
+              <div>
+                <h1 className="text-8xl ">Coffe Time</h1>
+                <h2 className="text-8xl ">And More...</h2>
+              </div>
+              <button className="bg-green-star sm:py-2 sm:px-5 rounded-full w-44 cursor-pointer text-white buttonHero">
+                Drink
+              </button>
+            </div>
           </div>
-          <NavbarCollapse className="uppercase">
-            <NavbarLink className="text-green-star! border-none hover:text-white! pl-5" href="#">Menu</NavbarLink>
-            <NavbarLink className="text-green-star! border-none hover:text-white! pl-5" href="#">Rewards</NavbarLink>
-            <NavbarLink className="text-green-star! border-none hover:text-white! pl-5" href="#">Gift Card</NavbarLink>
-            <NavbarLink className="text-green-star! border-none hover:text-white!" href="#"><Button className="cursor-pointer uppercase text-black! hover:text-white! outline-1 outline-black hover:outline-white bg-transparent!" pill>Find Store</Button></NavbarLink>
-          </NavbarCollapse>
-        </Navbar>
-      </header>
-      <section></section>
+          <article className="w-2/3">
+            <img className="w-max" src="/images/descarga.jpg" />
+          </article>
+        </article>
+      </section>
+      <section className="bg-green-star flex-grow w-full">
+        <article className="container m-auto text-white uppercase py-10">
+          <div className="w-max flex flex-col items-start justify-center space-y-8">
+            <p className="w-80">
+              Crave-worthy pairings including our new, best-ever iced coffee.
+              Restrictions apply.
+            </p>
+            <button className="uppercase underline flex justify-center items-center gap-3 cursor-pointer  group">
+              See more{" "}
+              <FaArrowRight className="group-hover:ml-2 transition-all duration-200" />
+            </button>
+          </div>
+        </article>
+      </section>
     </main>
   );
 }
